150a6564d303d7d74b8e85c73482321470138539
Santosh Patel <91826601+PatelSanto@users.noreply.github.com>
2024-08-28 21:29:42 +0530

    Changes in Home Screen

--- lib/users/screens/home_screen.dart
+++ lib/users/screens/home_screen.dart
@@ -11,33 +11,105 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leadingWidth: 80,
-        toolbarHeight: 100,
-        leading: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.notes_sharp, color: Colors.black, size: 50),
-        ),
-        actions: <Widget>[
-          CircleAvatar(
-            backgroundImage: AssetImage('assets/images/profile.png'),
-            radius: 35,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 10,
           ),
+          _profilerow(),
+          const SizedBox(height: 15),
+          _search()
         ],
-        backgroundColor: Color(0xFFFAFAFA),
       ),
-      body: _body(),
     );
   }
 
-  Widget _body() {
-    return Container(
-      padding: EdgeInsets.only(left: 25, top: 20),
-      child: Text(
-        'Blogs',
-        style: TextStyle(
-            color: Colors.black, fontSize: 40, fontWeight: FontWeight.bold),
-      ),
+// Top Row for Profile and Menu Bar
+  Widget _profilerow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.notes_sharp,
+              color: Colors.black,
+              size: 50,
+            )),
+        GestureDetector(
+          onTap: () {},
+          child: const CircleAvatar(
+            backgroundColor: Colors.blueGrey,
+            radius: 38,
+            child: CircleAvatar(
+              backgroundImage: AssetImage('assets/images/profile.png'),
+              radius: 35,
+            ),
+          ),
+        ),
+      ],
     );
   }
+}
+
+// For Search Bar
+Widget _search() {
+  return Column(
+    children: [
+      Container(
+        padding: const EdgeInsets.only(left: 20, top: 10),
+        alignment: AlignmentDirectional.topStart,
+        child: const Text(
+          'Blogs',
+          style: TextStyle(
+              fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+      ),
+      const SizedBox(height: 30),
+      Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 40, right: 40),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: const [
+                      BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 5,
+                          spreadRadius: 5,
+                          offset: Offset(5, 5))
+                    ]),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      hintText: 'Search Blogs',
+                      border: InputBorder.none,
+                      prefixIcon: Icon(Icons.search)),
+                ),
+              ),
+            ),
+          ),
+          //  SizedBox(width: 1),
+          Container(
+              decoration: const BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.rectangle,
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey,
+                        blurRadius: 5,
+                        spreadRadius: 5,
+                        offset: Offset(5, 5))
+                  ]),
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Icon(Icons.sort_sharp),
+              )),
+        ],
+      ),
+    ],
+  );
 }
